317f9e18717b5e9d9bef1136cee331e04721c4e6
VkHyperNova <kiis.veikko@gmail.com>
2022-11-09 15:32:05 +0200

    Contact page design, email, Google maps

--- front-page.php
+++ front-page.php
@@ -6,9 +6,9 @@
   <div class="container">
 
     <!-- Section Meist -->
-    <div class="my-5">
-      <h1 class="text-center"><?php the_field('firma_kirjelduse_pealkiri'); ?></h1>
-      <div class="description">
+    <div class="box my-5">
+      <h1 class="text-center pt-5"><?php the_field('firma_kirjelduse_pealkiri'); ?></h1>
+      <div class="description p-5">
         <p><?php the_field('firma_kirjeldus'); ?></p>
       </div>
     </div>
@@ -21,10 +21,8 @@
         <img class="frontpage-image-small image-rotate-left" src="<?php the_field('pilt-1'); ?>" alt="" />
       </div>
       <div class="col-6 my-auto px-5">
-        <!-- <h1>Säästlik mõtlemine!</h1> -->
         <h1><?php the_field('pilt-1-pealkiri'); ?></h1>
         <div class="description">
-          <!-- <p>30% Elektrienergiast saame päikese paneelidest!</p> -->
           <p><?php the_field('pilt-1-kirjeldus'); ?></p>
         </div>
       </div>
@@ -34,9 +32,7 @@
     <div class="row my-5 py-5">
       <div class="col-6 my-auto px-5">
         <h1><?php the_field('pilt-2-pealkiri'); ?></h1>
-        <!-- <h1>Puhas Pesu!</h1> -->
         <div class="description">
-          <!-- <p>Kaasaegsed tööstuslikud pesumasinad.</p> -->
           <p><?php the_field('pilt-2-kirjeldus'); ?></p>
         </div>
       </div>
@@ -52,11 +48,9 @@
         <img class="frontpage-image-small image-rotate-left" src="<?php the_field('pilt-3'); ?>" alt="" />
       </div>
       <div class="col-6 my-auto px-5">
-        <!-- <h1>Kalandriga triikimine!</h1> -->
         <h1><?php the_field('pilt-3-pealkiri'); ?></h1>
         <div class="description">
           <p><?php the_field('pilt-3-kirjeldus'); ?></p>
-          <!-- <p>Kalander on pesu triikimisseade, mis kasutab kuumuse ja auru koosmõju ülikvaliteetseks silumiseks</p> -->
         </div>
       </div>
     </div>
@@ -65,10 +59,8 @@
     <!-- Section 4 -->
     <div class="row my-5 py-5">
       <div class="col-6 my-auto px-5">
-        <!-- <h1>Konteiner</h1> -->
         <h1><?php the_field('pilt-4-pealkiri'); ?></h1>
         <div class="description my-3">
-          <!-- <p>Vajadusel saame pakkida pesu konteineritesse.</p> -->
           <p><?php the_field('pilt-4-kirjeldus'); ?></p>
         </div>
       </div>
@@ -83,10 +75,8 @@
         <img class="frontpage-image-small image-rotate-left" src="<?php the_field('pilt-5'); ?>" alt="" />
       </div>
       <div class="col-6 my-auto px-5">
-        <!-- <h1>Paindlik Teenindus!</h1> -->
         <h1><?php the_field('pilt-5-pealkiri'); ?></h1>
         <div class="description">
-          <!--  <p>Kiire ja kvaliteetne transport.</p> -->
           <p><?php the_field('pilt-5-kirjeldus'); ?></p>
         </div>
       </div>
--- page-kontakt.php
+++ page-kontakt.php
@@ -18,8 +18,7 @@
 
     <!-- Aadress -->
     <div class="col-sm-4">
-
-      <div class="contact-card">
+      <div class="box">
         <div class="px-5 py-5">
           <i class="fa-solid fa-house"></i>
         </div>
@@ -31,7 +30,7 @@
 
     <!-- Phone -->
     <div class="col-sm-4">
-      <div class="contact-card">
+      <div class="box">
         <div class="px-5 py-5">
           <i class="fas fa-phone-square-alt"></i>
         </div>
@@ -43,7 +42,7 @@
 
     <!-- Reg -->
     <div class="col-sm-4">
-      <div class="contact-card">
+      <div class="box">
         <div class="px-5 py-5">
           <i class="fas fa-address-card"></i>
         </div>
@@ -54,30 +53,64 @@
     </div>
   </div>
 
-  <!-- Google Maps -->
-  <div class="mapouter">
-    <div class="gmap_canvas">
-      <iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=ikomix&t=&z=15&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
-      <a href="https://fmovies-online.net">fmovies</a><br>
-      <style>
-        .mapouter {
-          position: relative;
-          text-align: right;
-          height: 500px;
-          width: 600px;
-        }
-      </style><a href="https://www.embedgooglemap.net"></a>
-      <style>
-        .gmap_canvas {
-          overflow: hidden;
-          background: none !important;
-          height: 500px;
-          width: 600px;
-        }
-      </style>
-    </div>
-  </div>
+  <!-- Email -->
+  <section class="mb-4 p-5 my-5">
+    <h2 class="h1-responsive font-weight-bold text-center my-4">Küsi Hinnapakkumist!</h2>
+    <div class="row">
+        <div class="col-md-9 mb-md-0 mb-5 custom-center-margin">
+        <div class="box">
+            <form id="contact-form" name="contact-form" action="mail.php" method="POST">
+                <div class="row pt-5 px-4">
+                    <div class="col-md-6">
+                        <div class="md-form mb-4">
+                            <input type="text" id="name" name="name" class="form-control" placeholder="Nimi">
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="md-form mb-4">
+                            <input type="text" id="email" name="email" class="form-control" placeholder="Email">
+                        </div>
+                    </div>
+                </div>
+                <div class="row px-4">
+                    <div class="col-md-12">
+                        <div class="md-form mb-4">
+                            <input type="text" id="subject" name="subject" class="form-control" placeholder="Pealkiri">
+                        </div>
+                    </div>
+                </div>
+                <div class="row px-4">
+                    <div class="col-md-12">
+                        <div class="md-form">
+                            <textarea type="text" id="message" name="message" rows="10" class="form-control md-textarea" placeholder="Sõnum"></textarea>
+                        </div>
+                    </div>
+                </div>
+            </form>
 
+            <div class="text-right my-4 pb-4 px-4">
+                <a class="btn " onclick="document.getElementById('contact-form').submit();">Saada Kiri</a>
+            </div>
+            <div class="status"></div>
+        </div>
+        <!--Grid column-->
+        </div>
+
+
+    </div>
+
+</section>
+
+  <!-- Google Maps -->
+  <div class="my-5">
+    <h2 class="pb-5 text-center">Meie Asukoht Kaardil</h2>
+    <div class="mapouter">
+      <div class="gmap_canvas"><iframe width="1100" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=ikomix&t=&z=15&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+      </div>
+    </div>
+
+  </div>
+  <img class="box" width="1100px" alt="Ikomix" src="<?php echo get_template_directory_uri(); ?>/img/ikomix-maja.jpg">
 
 </div>
 
