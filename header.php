276b5353463ed6e38f6499351f094265bd8c4d00
VkHyperNova <kiis.veikko@gmail.com>
2022-12-07 14:15:34 +0200

    favicon added

--- header.php
+++ header.php
@@ -4,6 +4,12 @@
 <head>
     <?php wp_head(); ?>
     <meta content="width=device-width, initial-scale=1" name="viewport" />
+    <link rel="android-chrome-192x192" sizes="192x192" href="<?php echo get_template_directory_uri(); ?>/img/android-chrome-192x192.png">
+    <link rel="android-chrome-512x512" sizes="512x512" href="<?php echo get_template_directory_uri(); ?>/img/android-chrome-512x512.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/img/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri(); ?>/img/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/img/favicon-16x16.png">
+    <link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/img/site.webmanifest">
 </head>
 
 <body <?php body_class(); ?>>
